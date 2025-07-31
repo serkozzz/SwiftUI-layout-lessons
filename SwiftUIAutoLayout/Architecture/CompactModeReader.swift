c4b4020b2fa16850413dd8c9581d1c0439ab12d5
Sergey Kozlov <s.kozlov@blackmana.com>
2025-07-31 16:32:23 +0300

    wip

--- SwiftUIAutoLayout/Architecture/CompactModeReader.swift
+++ SwiftUIAutoLayout/Architecture/CompactModeReader.swift
@@ -12,6 +12,11 @@ private func isCompactMode(vertical: SwiftUI.UserInterfaceSizeClass?) -> Bool {
     UIDevice.current.userInterfaceIdiom == .phone && vertical == .regular
 }
 
+
+//пример контейнера который инкапсулирует логику, которая может быть только во вью(зависимость от environment)
+//при этом не может быть в расширении или просто глоб фун-ции из за необходимости объявить хранимое св-во
+//результат работы этой логики передается в качестве параметра в замыкание которое получает контейнер.
+
 struct CompactModeReader<Content: View>: View {
     @Environment(\.verticalSizeClass) var verticalSizeClass
     
@@ -32,16 +37,23 @@ struct CompactModeReaderExample: View {
                 if (isCompactMode) {
                     VStack {
                         Image(systemName: "plus").resizable().aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: .infinity)
                         Image(systemName: "minus").resizable().aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: .infinity)
                     }
                 }
                 else {
                     HStack {
                         Image(systemName: "plus").resizable().aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: .infinity)
                         Image(systemName: "minus").resizable().aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: .infinity)
                     }
                 }
             }
         }
     }
 }
+
+
+
