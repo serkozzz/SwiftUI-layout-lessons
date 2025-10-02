712239ab6c6198f10386f00b0e8a800d868d20a4
Sergey Kozlov <s.kozlov@blackmana.com>
2025-10-02 06:31:00 +0300

    wip

--- SwiftUIAutoLayout/Strict Concurency/SingletonWithStrictConcurrency.swift
+++ SwiftUIAutoLayout/Strict Concurency/SingletonWithStrictConcurrency.swift
@@ -10,6 +10,10 @@ import SwiftUI
 private actor MyCar  {
     static let shared = MyCar()
     var speed: Int = 100
+    
+    func setSpeed(speed: Int) async {
+        self.speed = speed
+    }
 }
 
 struct SingletonWithStrictConcurrencySample: View {
@@ -19,9 +23,10 @@ struct SingletonWithStrictConcurrencySample: View {
             Text("Hello")
         }
         .onAppear() {
-            MyCar.shared.speed = 10
-            print(99)
-            print("MyCar.shared \(MyCar.shared.speed)")
+            Task {
+                await MyCar.shared.setSpeed(speed: 99)
+                print("MyCar.shared \(await MyCar.shared.speed)")
+            }
         }
     }
 }
